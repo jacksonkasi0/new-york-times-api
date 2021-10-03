e16d5bfdfb378a4a7ba963960d106950a8ac4f15
Jackson Kasi <68270206+jacksonkasi0@users.noreply.github.com>
2021-10-03 16:25:54 -0700

    bug fix!

--- script.js
+++ script.js
@@ -23,144 +23,150 @@ let bgColor = [
   "danger"
 ];
 
-num.forEach(i => {
-  topic = topicArr[i];
+// break;
 
-  async function getData() {
-    let url = `https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=Vw1t790RT5yLalH8YDrpO4kTtQlfyGiM`;
-    let responce = await fetch(url);
-    let data = await responce.json();
-    let result = data.results.map(item => item);
+let GetData = () => {
+  num.forEach(i => {
+    topic = topicArr[i];
 
-    //   --------------- color ----------->
-    let color = bgColor[Math.floor(Math.random() * bgColor.length)];
+    async function getData() {
+      let url = `https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=Vw1t790RT5yLalH8YDrpO4kTtQlfyGiM`;
+      let responce = await fetch(url);
+      let data = await responce.json();
+      let result = data.results.map(item => item);
 
-    let mydate = new Date();
-    // ------------ Date -------->
-    let value = `${result[0].published_date}`;
+      //   --------------- color ----------->
+      let color = bgColor[Math.floor(Math.random() * bgColor.length)];
 
-    const [date, year] = [mydate.getDate(value), mydate.getFullYear(value)];
+      let mydate = new Date();
+      // ------------ Date -------->
+      let value = `${result[0].published_date}`;
 
-    const formatter = new Intl.DateTimeFormat("en", { month: "short" });
-    const month = formatter.format(new Date(value));
+      const [date, year] = [mydate.getDate(value), mydate.getFullYear(value)];
 
-    // ---------
-    const published_date = document.createElement("span");
-    published_date.className = "badge text-white date";
-    published_date.innerText = `${month} ${date}, ${year}`;
+      const formatter = new Intl.DateTimeFormat("en", { month: "short" });
+      const month = formatter.format(new Date(value));
 
-    if (i <= 4) {
-      // -------------------------//
+      // ---------
+      const published_date = document.createElement("span");
+      published_date.className = "badge text-white date";
+      published_date.innerText = `${month} ${date}, ${year}`;
 
-      let Card = document.querySelector(`.box-${i}`);
-      Card.style.cssText += `background-image: url(${result[0].multimedia[0].url})`;
+      if (i <= 4) {
+        // -------------------------//
 
-      let boxinfo = document.querySelector(`.box1-info${i}`);
+        let Card = document.querySelector(`.box-${i}`);
+        Card.style.cssText += `background-image: url(${result[0].multimedia[0].url})`;
 
-      let link = document.createElement("a");
-      link.href = `${result[0].url}`;
-      boxinfo.appendChild(link);
+        let boxinfo = document.querySelector(`.box1-info${i}`);
 
-      let badge = document.createElement("div");
-      badge.className = `badge text-white bg-${color}`;
+        let link = document.createElement("a");
+        link.href = `${result[0].url}`;
+        boxinfo.appendChild(link);
 
-      let a = document.createElement("a");
-      badge.appendChild(a);
+        let badge = document.createElement("div");
+        badge.className = `badge text-white bg-${color}`;
 
-      let ico = document.createElement("i");
-      ico.className = "fas fa-circle  me-2 small fw-bold";
-      ico.innerText = `    ${result[0].section}`;
-      a.appendChild(ico);
+        let a = document.createElement("a");
+        badge.appendChild(a);
 
-      let title = document.createElement("h4");
-      title.className = "card-title";
-      title.innerText = `${result[0].title}`;
+        let ico = document.createElement("i");
+        ico.className = "fas fa-circle  me-2 small fw-bold";
+        ico.innerText = `    ${result[0].section}`;
+        a.appendChild(ico);
 
-      let discription = document.createElement("p");
-      discription.innerText = `${result[0].abstract}`;
+        let title = document.createElement("h4");
+        title.className = "card-title";
+        title.innerText = `${result[0].title}`;
 
-      let author = document.createElement("span");
-      author.className = "me-2 author";
-      author.innerText = `${result[0].byline}`;
+        let discription = document.createElement("p");
+        discription.innerText = `${result[0].abstract}`;
 
-      link.append(
-        badge,
-        title,
-        i === 1 ? discription : "",
-        author,
-        published_date
-      );
-    }
+        let author = document.createElement("span");
+        author.className = "me-2 author";
+        author.innerText = `${result[0].byline}`;
 
-    //   ------------------ midle layer ---------------//
-    let hilights = document.querySelector(".bi-hourglass-split");
-    hilights.classList.add(`text-${color}`);
+        link.append(
+          badge,
+          title,
+          i === 1 ? discription : "",
+          author,
+          published_date
+        );
+      }
 
-    if (i >= 5) {
-      //   -------------------
+      //   ------------------ midle layer ---------------//
+      let hilights = document.querySelector(".bi-hourglass-split");
+      hilights.classList.add(`text-${color}`);
 
-      let midle_news = document.querySelector(".midle-news");
+      if (i >= 5) {
+        //   -------------------
 
-      //   ----------- main card ---------->>>
+        let midle_news = document.querySelector(".midle-news");
 
-      let midle_news_cards = document.createElement("div");
-      midle_news_cards.className = "card  col-md-2";
-      midle_news.appendChild(midle_news_cards);
+        //   ----------- main card ---------->>>
 
-      //  ---------------- card image ----------------->>>
-      let midle_card_img = document.createElement("div");
-      midle_card_img.className = `midle-card-img`;
-      midle_card_img.style.cssText += `background-image: url(${result[0].multimedia[0].url})`;
-      midle_news_cards.appendChild(midle_card_img);
-      // ----------- card badge ---------
+        let midle_news_cards = document.createElement("div");
+        midle_news_cards.className = "card  col-md-2";
+        midle_news.appendChild(midle_news_cards);
 
-      let midle_card_badge = document.createElement("div");
-      midle_card_badge.className = `badge bg-${color}`;
-      midle_card_img.appendChild(midle_card_badge);
-      //  --------
-      let ico = document.createElement("i");
-      ico.className = "fas fa-circle  me-2 small fw-bold";
-      ico.innerText = `    ${result[0].section}`;
-      midle_card_badge.appendChild(ico);
+        //  ---------------- card image ----------------->>>
+        let midle_card_img = document.createElement("div");
+        midle_card_img.className = `midle-card-img`;
+        midle_card_img.style.cssText += `background-image: url(${result[0].multimedia[0].url})`;
+        midle_news_cards.appendChild(midle_card_img);
+        // ----------- card badge ---------
 
-      // ---------------- card body ----------->>>
-      let midle_card_body = document.createElement("div");
-      midle_card_body.className = "card-body";
-      midle_news_cards.appendChild(midle_card_body);
-      // --------------- card head line -------
+        let midle_card_badge = document.createElement("div");
+        midle_card_badge.className = `badge bg-${color}`;
+        midle_card_img.appendChild(midle_card_badge);
+        //  --------
+        let ico = document.createElement("i");
+        ico.className = "fas fa-circle  me-2 small fw-bold";
+        ico.innerText = `    ${result[0].section}`;
+        midle_card_badge.appendChild(ico);
 
-      let link = document.createElement("a");
-      link.href = `${result[0].url}`;
+        // ---------------- card body ----------->>>
+        let midle_card_body = document.createElement("div");
+        midle_card_body.className = "card-body";
+        midle_news_cards.appendChild(midle_card_body);
+        // --------------- card head line -------
 
-      let head_line = document.createElement("h5");
-      head_line.className = "card-title text-dark";
-      head_line.innerText = `${result[0].title}`;
-      link.appendChild(head_line);
-      // --------------- card apstract {p-tag} -----------
-      let p = document.createElement("p");
-      p.className = "card-text";
-      p.innerText = `${result[0].abstract}`;
+        let link = document.createElement("a");
+        link.href = `${result[0].url}`;
 
-      //  ----------- author & time -------->
+        let head_line = document.createElement("h5");
+        head_line.className = "card-title text-dark";
+        head_line.innerText = `${result[0].title}`;
+        link.appendChild(head_line);
+        // --------------- card apstract {p-tag} -----------
+        let p = document.createElement("p");
+        p.className = "card-text";
+        p.innerText = `${result[0].abstract}`;
 
-      let author = document.createElement("span");
-      author.className = "me-2 author text-secondary";
-      author.innerText = `${result[0].byline}`;
+        //  ----------- author & time -------->
 
-      // -----------
+        let author = document.createElement("span");
+        author.className = "me-2 author text-secondary";
+        author.innerText = `${result[0].byline}`;
 
-      midle_card_body.append(link, p, author, published_date);
+        // -----------
 
-      if (num.at(-1) === i) {
-        let loadButton = document.querySelector(".loadBtn");
-        let loadBtn = document.createElement("div");
-        loadBtn.className = "btn mx-auto  btn-primary text-white";
-        loadBtn.innerHTML =
-          'Load more post <i class="far fa-arrow-alt-circle-down"></i>';
-        loadButton.appendChild(loadBtn);
+        midle_card_body.append(link, p, author, published_date);
+
+        if (num.at(-1) === i) {
+          let loadButton = document.querySelector(".loadBtn");
+          let loadBtn = document.createElement("div");
+          loadBtn.className = "btn mx-auto  btn-primary text-white";
+          loadBtn.innerHTML =
+            'Load more post <i class="far fa-arrow-alt-circle-down"></i>';
+          loadButton.appendChild(loadBtn);
+        }
       }
     }
-  }
 
-  getData();
-});
+    getData();
+  });
+};
+
+GetData();
